bf6cf5cfb7a8b28942b1da92c5f604d3381dabe4
노경민 <skcc70293@sk.com>
2017-08-01 09:30:30 +0900

    헤더 내비게이션바로 변경

--- main-application/src/containers/Header.js
+++ main-application/src/containers/Header.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-import HeaderButton from '../components/HeaderButton'
-import HeaderNav from '../components/HeaderNav'
+import HeaderNavbar from '../components/HeaderNavbar';
 
 import './Header.css'
 
 class Header extends Component{
   render() {
     return (
-      <div className="Header">
-        <Container fluid={true}>
-          <Row>
-            <Col md="6">
-            </Col>
-            <Col md="3">
-            </Col>
-            <Col xs="12" md="3">
-              <HeaderButton to="/login" name="로그인"/>{'  '}
-              <HeaderButton to="/signup" name="회원가입"/>
-            </Col>
-          </Row>
+      <div>
+        <HeaderNavbar/>
+        <Container className="Header" fluid={true}>
               <h1>Smart Meeting Assistant</h1>
-          <Row>
-          </Row>
-          <Row>
-            <Col md="3">
-              <HeaderNav/>
-            </Col>
-          </Row>
         </Container>
       </div>
     );
